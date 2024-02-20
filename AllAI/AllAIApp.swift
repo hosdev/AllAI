aabd3d95171ce24e1a7700cbf3f563c8d455398a
hosam al-deen abufasha <hosamabufashacr7@hotmail.com>
2024-02-20 12:39:37 +0300

    Initial Commit

---
+++ AllAI/AllAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AllAIApp.swift
+//  AllAI
+//
+//  Created by hosam abufasha on 20/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct AllAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
